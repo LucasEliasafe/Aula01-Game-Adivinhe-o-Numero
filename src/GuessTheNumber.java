497ab269827e89d60927b4b6939f5cda74cbedf9
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-08-21 21:34:24 -0300

    Feat: Criar método para gerar número aleatórios e iniciar o jogo

---
+++ src/GuessTheNumber.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    private static Scanner scanner = new Scanner(System.in);
+
+
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+        startGame();
+    }
+
+
+}
