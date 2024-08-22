176070c051e194839c918acaa64c5a860bc15a8f
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-08-21 21:39:28 -0300

    Feat: Adicionar menu com as dificuldades do usuário

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -11,5 +11,31 @@ public class GuessTheNumber {
         startGame();
     }
 
+    private static void startGame() {
+        System.out.println("Bem-vindo ao jogo Adivinhe o Número!");
+        System.out.println("Escolha uma dificuldade");
+        System.out.println("1. Fácil (1-10");
+        System.out.println("2. Médio (1-50)");
+        System.out.println("3. Difícil (1-100)");
+
+        int difficulty = scanner.nextInt();
+
+        switch (difficulty) {
+            case 1:
+                playGame(10);
+                break;
+            case 2:
+                playGame(50);
+                break;
+            case 3:
+                playGame(100);
+                break;
+            default:
+                System.out.println("Escolha inválida. Tente novamente.");
+                startGame();
+                break;
+        }
+
+    }
 
 }
